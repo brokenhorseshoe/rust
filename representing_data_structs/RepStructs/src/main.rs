e8fbc91066bdc8cfccd78ab5ba835717404b0413
David <brokenhorseshoe@gmail.com>
2024-08-08 13:09:00 -0400

    first

---
+++ representing_data_structs/RepStructs/src/main.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     print!("Hello, world!");
+// }
+// notes 
+// fn represents a function
+// main is the entry point of the program
+// print! is a macro that prints text to the console
+// println! is a macro that prints text to the console
+// the differance between print! and println! is that println! adds a new line at the end of the text
+// in rust ' is a char " is a string"
+
+// Structs are used to create custom data types
+// to create an object like a class in other languages
